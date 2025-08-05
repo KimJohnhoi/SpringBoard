da85d52390be9826e46e8341fdcec937cccef896
KimJohnhoi <johnhoi99@gmail.com>
2025-08-05 12:25:05 +0900

    Board Ver 1.1

--- src/main/java/com/board/controller/TestController.java
+++ src/main/java/com/board/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
-
+	// http://localhost:9090/root
 	@RequestMapping("/")
 	public String home() {
 		 return "home"; 
